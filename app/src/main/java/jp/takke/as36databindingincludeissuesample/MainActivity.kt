66e2c405a89a14cf6b34121194dc68d94e08781e
takke <hiroaki@v7.com>
2020-02-29 15:16:02 +0900

    add DataBinding

--- app/src/main/java/jp/takke/as36databindingincludeissuesample/MainActivity.kt
+++ app/src/main/java/jp/takke/as36databindingincludeissuesample/MainActivity.kt
@@ -2,11 +2,19 @@ package jp.takke.as36databindingincludeissuesample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import jp.takke.as36databindingincludeissuesample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+
+        binding.sampleText.text = "Hello Binding!"
     }
 }
